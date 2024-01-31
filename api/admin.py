f65674ab353ab91cdfeea5824c4f35efd76d0c9c
Mark Domahidi <mdomahidi@gmail.com>
2024-01-31 14:20:14 +0100

    feat: customize admin site interface for spendings

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+
+from .models import Spending
+
+
+class SpendingAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["description"]}),
+        (None, {"fields": ["amount"]}),
+        (None, {"fields": ["spent_at"]}),
+        (None, {"fields": ["currency"]}),
+    ]
+    list_display = ["description", "amount", "currency", "spent_at"]
+    list_filter = ["currency"]
+    search_fields = ["description"]
+
+
+admin.site.register(Spending, SpendingAdmin)
