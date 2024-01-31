541a6720b973162643894de145a245c28ddad4b3
Mark Domahidi <mdomahidi@gmail.com>
2024-01-31 14:18:47 +0100

    feat: create spending model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+
+class Spending(models.Model):
+    class Currency(models.TextChoices):
+        HUF = "HUF", "HUF"
+        USD = "USD", "USD"
+
+    description = models.CharField(max_length=255)
+    amount = models.FloatField()
+    spent_at = models.DateTimeField()
+    currency = models.CharField(max_length=3, choices=Currency.choices)
+
+    def __str__(self):
+        return self.description
